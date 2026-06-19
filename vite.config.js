860a4b50c991765f11b0a2094ea00b24bcdb2b08
Henrique Gonzalez <henriquegonzalez@poli.ufrj.br>
2026-06-19 14:27:19 -0300

    Adiciona página de vendas e ajusta rotas de build para manter url da Aula Magna intacta

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        vendas: resolve(__dirname, 'pagina-de-vendas.html')
+      }
+    }
   }
 });
