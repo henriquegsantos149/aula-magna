f7ba6797c336bbfe10395143285fc7d4d6591e32
olivio-pro <olivio@ambientalpro.com.br>
2026-08-11 12:31:52 -0300

    Atualiza prefixo de UTM no script da GGSR para refletir a tag AM

--- ggsr/script.js
+++ ggsr/script.js
@@ -449,10 +449,10 @@ function initFormControls() {
     
     urlParams.forEach((val, key) => {
       const lowerKey = key.toLowerCase();
-      if (lowerKey.startsWith('utm_') || lowerKey.startsWith('l19psggsr_utm_')) {
+      if (lowerKey.startsWith('utm_') || lowerKey.startsWith('amposggsr_utm_')) {
         let normalizedKey = lowerKey;
-        if (lowerKey.startsWith('l19psggsr_utm_')) {
-          normalizedKey = lowerKey.replace('l19psggsr_', '');
+        if (lowerKey.startsWith('amposggsr_utm_')) {
+          normalizedKey = lowerKey.replace('amposggsr_', '');
         }
         
         // Passar os UTMs limpos para o Tally também
